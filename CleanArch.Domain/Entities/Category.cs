04ea5d71ad057f57908bef19bca5d89396dbfd4c
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-08-22 17:48:05 -0300

    feat: create base entities

---
+++ CleanArch.Domain/Entities/Category.cs
@@ -0,0 +1,43 @@
+﻿using CleanArch.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Entities
+{
+	public sealed class Category
+	{
+		public int Id { get; private set; }
+		public string Name { get; private set; }
+
+		public Category(string name)
+		{
+			ValidateDomain(name);
+		}
+		public Category(int id, string name)
+		{
+			DomainExceptionValidation.When(id < 0, "Invalid id");
+			Id = id;
+			ValidateDomain(name);
+		}
+
+		public ICollection<Product> Products { get; set; }
+
+		private void ValidateDomain(string name)
+		{
+			DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+				"Invalid name. Name is required");
+
+			DomainExceptionValidation.When(name.Length < 3,
+				"Invalid name. Too short, minimum 3 characters");
+
+			Name = name;
+		}
+		public void Update(string name)
+		{
+			ValidateDomain(name);
+		}
+	}
+}
