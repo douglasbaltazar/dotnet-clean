8c89f3e8ea254101cfc84b86f80fb6cc5e1ef409
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-08-23 14:41:41 -0300

    fix: remove unused imports

--- CleanArch.Application/Interfaces/IProductService.cs
+++ CleanArch.Application/Interfaces/IProductService.cs
@@ -1,15 +1,12 @@
 ﻿using CleanArch.Application.DTOs;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArch.Application.Interfaces
 {
 	public interface IProductService
 	{
-		Task<IEnumerable<ProductDTO>> GetProduct();
+		Task<IEnumerable<ProductDTO>> GetProducts();
 		Task<ProductDTO> GetById(int? id);
 
 		Task<ProductDTO> GetProductCategory(int? id);
