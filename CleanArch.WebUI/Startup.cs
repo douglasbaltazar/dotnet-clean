16c73c222ddf7f4339e7faa8cf34f5c3e3c6c209
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-08-23 15:22:34 -0300

    feat: created CategoriesController to return data in json

--- CleanArch.WebUI/Startup.cs
+++ CleanArch.WebUI/Startup.cs
@@ -49,9 +49,7 @@ namespace CleanArch.WebUI
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapControllerRoute(
-					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
+				endpoints.MapControllers();
 			});
 		}
 	}
