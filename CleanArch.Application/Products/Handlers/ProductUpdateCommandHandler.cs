8825ab9fb5898e2c505082c04e2f5d587c46182b
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-08-23 16:11:14 -0300

    feat: created ProductUpdateCommandHandler to handle update of a product

---
+++ CleanArch.Application/Products/Handlers/ProductUpdateCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using CleanArch.Application.Products.Commands;
+using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Products.Handlers
+{
+	public class ProductUpdateCommandHandler : IRequestHandler<ProductUpdateCommand, Product>
+	{
+		private readonly IProductRepository _productRepository;
+
+		public ProductUpdateCommandHandler(IProductRepository productRepository)
+		{
+			_productRepository = productRepository;
+		}
+		public async Task<Product> Handle(ProductUpdateCommand request, CancellationToken cancellationToken)
+		{
+			var product = await _productRepository.GetById(request.Id);
+
+			if (product == null)
+			{
+				throw new ApplicationException($"Error could not be found.");
+			}
+			else
+			{
+				product.Update(request.Name, request.Description, request.Price, request.Stock, request.Image, request.CategoryId);
+				return await _productRepository.Update(product);
+			}
+		}
+	}
+}
