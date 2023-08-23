0295eea8026f410cc9a98f9af8187e969d6d19fd
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-08-23 19:12:21 -0300

    feat: finished ProductController

--- CleanArch.WebUI/Controllers/ProductsController.cs
+++ CleanArch.WebUI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using CleanArch.Application.Interfaces;
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,32 @@ namespace CleanArch.WebUI.Controllers
 		{
 			var products = await _productService.GetProducts();
 			return Json(products);
+		}
+
+		[HttpPost]
+		[Route("new")]
+		public async Task<IActionResult> CreateProduct([FromBody] ProductDTO data)
+		{
+			await _productService.Add(data);
+			return Json(data);
+		}
+
+		[HttpPut]
+		[Route("{id}")]
+		public async Task<IActionResult> UpdateProduct(int id, [FromBody] ProductDTO data)
+		{
+			data.Id = id;
+			await _productService.Update(data);
+			return Json(data);
+		}
+
+		[HttpDelete]
+		[Route("{id}")]
+		public async Task<IActionResult> DeleteProduct(int id)
+		{
+			var data = await _productService.GetById(id);
+			await _productService.Remove(id);
+			return Json(data);
 		}
 	}
 }
