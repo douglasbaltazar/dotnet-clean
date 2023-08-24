b7d9760cfe9f0f110a744a83be6c66835f36602e
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-08-24 15:49:10 -0300

    feat: add HttpGet method to Categories controller

---
+++ CleanArch.API/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
+	{
+		private readonly ICategoryService _categorySerive;
+		public CategoriesController(ICategoryService categorySerive)
+		{
+			_categorySerive = categorySerive;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<CategoryDTO>>> List()
+		{
+			var categories = await _categorySerive.GetCategories();
+			if(categories == null)
+			{
+				return NotFound("Categories not found");
+			}
+			return Ok(categories);
+		} 
+	}
+}
